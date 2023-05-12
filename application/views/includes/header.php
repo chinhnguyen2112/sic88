9deea7ae975791d3f586b8140d4ef7c119faadb4
Hoàng Tuấn Khanh <agola3bie@gmail.com>
2023-05-12 14:42:21 +0700

    lan 2

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -4,10 +4,10 @@
             <div class="header_scroll">
                 <img src="/images/menu_mb.svg" alt="show menu" class="img_show_menu" onclick="show_menu(this,1)">
                 <div class="logo_header">
-                    <img src="images/icons/logo.png">
+                    <img src="/images/icons/logo.png">
                 </div>
                 <div class="search">
-                    <img class="img_search" src="images/icons/icons8-search-30.png">
+                    <img class="img_search" src="/images/icons/icons8-search-30.png">
                     <input placeholder="Search..." />
                 </div>
                 <div class="all_btn_acc">
@@ -17,13 +17,13 @@
                 <img class="search_nonpc" src="/images/icons/icons8-search-25.png">
             </div>
         </div>
-        <div class="menu">
+        <div class="header_menu">
             <div class="list_menu">
                 <div class="menu" id="menu">
                     <ul class="list_menu">
                         <li class="item_menu">
                             <a class="item_home" href="#">
-                                <img src="images/icons/icons_home.png">
+                                <img class="logo_home" src="/images/icons/icons_home.png">
                                 <p>TRANG CHỦ</p>
                             </a>
                         </li>
@@ -43,13 +43,13 @@
                         <?php } ?>
                         <li class="item_menu">
                             <a class="link_special" href="#">
-                                <img src="images/icons/icons_km.png">
+                                <img src="/images/icons/icons_km.png">
                                 <p>KHUYẾN MÃI</p>
                             </a>
                         </li>
                         <li class="item_menu">
                             <a href="#">
-                                <img src="images/icons/icons_vip.png">
+                                <img src="/images/icons/icons_vip.png">
                             </a>
                         </li>
                     </ul>
@@ -59,7 +59,7 @@
         <div class="header_bot" id="header_bot">
             <div class="header_scroll">
                 <div class="left_noti">
-                    <img src="images/icons/icons_tb.png">
+                    <img src="/images/icons/icons_tb.png">
                     <p class="title_noti">Thông Báo:</p>
                 </div>
                 <marquee>
