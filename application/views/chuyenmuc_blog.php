7bf87695233f39afe27dba0a983e5f2033c4f2f1
Hoàng Tuấn Khanh <agola3bie@gmail.com>
2023-05-30 08:36:53 +0700

    Khanh update code ngay 30/05/2023 lan 1

--- application/views/chuyenmuc_blog.php
+++ application/views/chuyenmuc_blog.php
@@ -2,8 +2,13 @@
     <div class="content_about body_width">
         <div class="banner_blog">
             <div class="breadcrumb">
-                <a href="/">Trang chủ</a>
-                <span> / </span>
+                <span>
+                    <img src="/images/icons/icons8-home-20.png" alt="icon home small">
+                    <a href="/">Trang chủ</a>
+                </span>
+                <span>
+                    <img src="/images/icons/icons8-arrow-right-10.png">
+                </span>
                 <span class="this_breadcrumb"><?= $title_page ?></span>
             </div>
         </div>
@@ -11,34 +16,34 @@
             <div class="top_blog">
                 <div class="top_left">
                     <?php if (isset($content_cate) && $content_cate != '') { ?>
-                        <div class="blog_content">
-                            <div class="left_detail">
-                                <div class="content_blog" id="content_blog">
-                                    <?= $content_cate ?>
-                                </div>
-                                <div class="hide_show_content">
-                                    <p class="hide_show_text">Xem thêm</p>
-                                    <div class="list_img_arrow">
-                                        <img src="/images/arrow.svg" alt="xem thêm">
-                                        <img src="/images/arrow.svg" alt="xem thêm">
-                                    </div>
+                    <div class="blog_content">
+                        <div class="left_detail">
+                            <div class="content_blog" id="content_blog">
+                                <?= $content_cate ?>
+                            </div>
+                            <div class="hide_show_content">
+                                <p class="hide_show_text">Xem thêm</p>
+                                <div class="list_img_arrow">
+                                    <img src="/images/arrow.svg" alt="xem thêm">
+                                    <img src="/images/arrow.svg" alt="xem thêm">
                                 </div>
                             </div>
                         </div>
+                    </div>
                     <?php } ?>
                     <div class="list_blog_top">
                         <div class="left_blog">
                             <div class="blog_top">
                                 <?php foreach ($blog as $key => $val) {
                                     if ($key == 0) { ?>
-                                        <a class="linl_all_detail" title="<?= $val['title'] ?>" href="/<?= $val['alias'] ?>/">
-                                            <img src="/<?= $val['image'] ?>" alt="<?= $val['title'] ?>">
-                                            <p class="title_blog_top"><?= $val['title'] ?></p>
-                                            <div class="fl_date">
-                                                <p class="date_post"><?= date('d-m-Y', $val['created_at']) ?></p>
-                                            </div>
-                                            <div class="sapo_blog_top"><?= $val['sapo'] ?></div>
-                                        </a>
+                                <a class="linl_all_detail" title="<?= $val['title'] ?>" href="/<?= $val['alias'] ?>/">
+                                    <img src="/<?= $val['image'] ?>" alt="<?= $val['title'] ?>">
+                                    <p class="title_blog_top"><?= $val['title'] ?></p>
+                                    <div class="fl_date">
+                                        <p class="date_post"><?= date('d-m-Y', $val['created_at']) ?></p>
+                                    </div>
+                                    <div class="sapo_blog_top"><?= $val['sapo'] ?></div>
+                                </a>
                                 <?php }
                                 } ?>
                             </div>
@@ -46,12 +51,12 @@
                         <div class="right_blog">
                             <?php foreach ($blog as $key => $val) {
                                 if ($key < 4 && $key > 0) { ?>
-                                    <div class="this_train_right">
-                                        <a class="linl_all_detail" title="<?= $val['title'] ?>" href="/<?= $val['alias'] ?>/">
-                                            <p class="title_blog"><?= $val['title'] ?></p>
-                                            <div class="des_blog"><?= $val['sapo'] ?></div>
-                                        </a>
-                                    </div>
+                            <div class="this_train_right">
+                                <a class="linl_all_detail" title="<?= $val['title'] ?>" href="/<?= $val['alias'] ?>/">
+                                    <p class="title_blog"><?= $val['title'] ?></p>
+                                    <div class="des_blog"><?= $val['sapo'] ?></div>
+                                </a>
+                            </div>
                             <?php }
                             } ?>
                         </div>
@@ -59,16 +64,16 @@
                     <div class="list_blog">
                         <?php foreach ($blog as $key => $val) {
                             if ($key > 4) { ?>
-                                <div class="this_train">
-                                    <a href="/<?= $val['alias'] ?>/">
-                                        <img src="/<?= $val['image'] ?>" alt="<?= $val['title'] ?>">
-                                        <div class="box_right_data">
-                                            <p class="title_blog"><?= $val['title'] ?></p>
-                                            <p class="date_post"><span><?= date('d-m-Y', $val['created_at']) ?></span></p>
-                                            <div class="des_blog"><?= $val['sapo'] ?></div>
-                                        </div>
-                                    </a>
+                        <div class="this_train">
+                            <a href="/<?= $val['alias'] ?>/">
+                                <img src="/<?= $val['image'] ?>" alt="<?= $val['title'] ?>">
+                                <div class="box_right_data">
+                                    <p class="title_blog"><?= $val['title'] ?></p>
+                                    <p class="date_post"><span><?= date('d-m-Y', $val['created_at']) ?></span></p>
+                                    <div class="des_blog"><?= $val['sapo'] ?></div>
                                 </div>
+                            </a>
+                        </div>
                         <?php }
                         } ?>
                         <div class="load_more">
--- application/views/detail_blog.php
+++ application/views/detail_blog.php
@@ -3,10 +3,17 @@
         <div class="main_content_blog">
             <div class="blog_content">
                 <div class="breadcrumb">
-                    <a href="/">Trang chủ</a>
-                    <span> / </span>
+                    <span>
+                        <img src="/images/icons/icons8-home-20.png" alt="icon home small">
+                        <a href="/">Trang chủ</a>
+                    </span>
+                    <span>
+                        <img src="/images/icons/icons8-arrow-right-10.png">
+                    </span>
                     <a class="link_breadcrumb" href="/<?= $cate_alias ?>"><?= $breadcrumb ?></a>
-                    <span> / </span>
+                    <span>
+                        <img src="/images/icons/icons8-arrow-right-10.png" src="icon arrow right">
+                    </span>
                     <span class="this_breadcrumb"><?= $blog['title'] ?></span>
                 </div>
                 <div class="box_data_blog">
@@ -35,30 +42,31 @@
                     </div>
                 </div>
                 <?php if ($blog_same != null) { ?>
-                    <div class="blog_same">
-                        <div class="list_blog_same">
-                            <?php
+                <div class="blog_same">
+                    <div class="list_blog_same">
+                        <?php
                             foreach ($blog_same as $val) { ?>
-                                <div class="this_handbook ">
-                                    <a class="img_item" href="/<?= $val['alias'] ?>/">
-                                        <img class="img_blog_same" src="/<?= $val['image'] ?>" alt="<?= $val['title'] ?>">
-                                    </a>
-                                    <div class="data_handbook">
-                                        <a class="title_handbook" href="/<?= $val['alias'] ?>"><?= $val['title'] ?></a>
-                                        <p class="date_post">
-                                            <a class="name_cate" href="/<?= $blog['alias_cate'] ?>"><?= $blog['name_cate'] ?></a>
-                                            <span>
-                                                <?= date('d-m-Y', $val['created_at']) ?>
-                                                <span>
-                                        </p>
-                                        <div class="this_des_handbook"><?= $val['sapo'] ?></div>
-                                    </div>
+                        <div class="this_handbook ">
+                            <a class="img_item" href="/<?= $val['alias'] ?>/">
+                                <img class="img_blog_same" src="/<?= $val['image'] ?>" alt="<?= $val['title'] ?>">
+                            </a>
+                            <div class="data_handbook">
+                                <a class="title_handbook" href="/<?= $val['alias'] ?>"><?= $val['title'] ?></a>
+                                <p class="date_post">
+                                    <a class="name_cate"
+                                        href="/<?= $blog['alias_cate'] ?>"><?= $blog['name_cate'] ?></a>
+                                    <span>
+                                        <?= date('d-m-Y', $val['created_at']) ?>
+                                        <span>
+                                </p>
+                                <div class="this_des_handbook"><?= $val['sapo'] ?></div>
+                            </div>
 
-                                </div>
-                            <?php
-                            } ?>
                         </div>
+                        <?php
+                            } ?>
                     </div>
+                </div>
                 <?php } ?>
                 <div class="line_blog"></div>
             </div>
